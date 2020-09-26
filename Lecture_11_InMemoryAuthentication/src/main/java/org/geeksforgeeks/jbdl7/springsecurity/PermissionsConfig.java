7d998e10a48d7443ffd2c1f4c18fc6d03dc1bcfc
Piyush Aggarwal <piyush5807@gmail.com>
2020-09-26 22:27:32 +0530

    added in memory authentication code

---
+++ Lecture_11_InMemoryAuthentication/src/main/java/org/geeksforgeeks/jbdl7/springsecurity/PermissionsConfig.java
@@ -0,0 +1,44 @@
+package org.geeksforgeeks.jbdl7.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PermissionsConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("piyush")
+                .password("piyush1234")
+                .authorities("ADMIN")
+                .and()
+                .withUser("rahul")
+                .password("rahul1234")
+                .authorities("USER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/user/**").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin();
+
+    }
+
+    @Bean
+    public PasswordEncoder getMyPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
